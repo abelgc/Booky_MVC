512f89e2f2fdb50ca35eda69984e9f22816bce7a
“abelgc” <abelcanizaresgalles@gmail.com>
2024-04-19 19:48:15 +0200

    Comments on program file

--- BookyWeb/Program.cs
+++ BookyWeb/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline side and is a middleware.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -20,6 +20,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Defines defaut route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
