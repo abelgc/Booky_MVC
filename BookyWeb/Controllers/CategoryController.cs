f4960dc2dc85893ed7282e90de39653c13779e56
“abelgc” <abelcanizaresgalles@gmail.com>
2024-05-03 13:33:14 +0200

    add category controller with view logic

--- BookyWeb/Controllers/CategoryController.cs
+++ BookyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext) {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categoryList = _dbContext.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
