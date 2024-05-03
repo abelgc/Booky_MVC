dc28c2179365b1d989d057484b7a93ec1cf109af
“abelgc” <abelcanizaresgalles@gmail.com>
2024-05-03 12:20:55 +0200

    Add Category table and category view

--- BookyWeb/Data/ApplicationDbContext.cs
+++ BookyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookyWeb.Data
 {
@@ -8,5 +9,7 @@ namespace BookyWeb.Data
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
