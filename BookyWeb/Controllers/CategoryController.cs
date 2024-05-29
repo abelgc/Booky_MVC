29b2de7678f3bb652a358ff58680fbef9bb5a9f9
“abelgc” <abelcanizaresgalles@gmail.com>
2024-05-29 11:39:59 +0200

    Add Edit and Delete methods

--- BookyWeb/Controllers/CategoryController.cs
+++ BookyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BookyWeb.Data;
 using BookyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookyWeb.Controllers
 {
@@ -17,6 +18,7 @@ namespace BookyWeb.Controllers
             return View(categoryList);
         }
 
+        //Get
         public IActionResult CreateCategory()
         {
             return View();
@@ -24,11 +26,6 @@ namespace BookyWeb.Controllers
         [HttpPost]
         public IActionResult CreateCategory(Category newCategory)
         {
-            if(string.IsNullOrEmpty(newCategory.Name))
-            {
-                ModelState.TryAddModelError("name", "Category cannot be empty");
-            }
-
             if (newCategory!.Name == newCategory.DisplayOrder.ToString())
             {
                 ModelState.TryAddModelError("name", "Category Name cannot be a number");
@@ -42,6 +39,77 @@ namespace BookyWeb.Controllers
             } 
             
             return View();
+           
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditCategory(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = await _dbContext.Categories.FirstOrDefaultAsync(u=>u.Id == id.Value);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+       
+        [HttpPost]
+        public IActionResult EditCategory(Category newCategory)
+        {
+            if (newCategory!.Name == newCategory.DisplayOrder.ToString())
+            {
+                ModelState.TryAddModelError("name", "Category Name cannot be a number");
+            }
+
+            if (ModelState.IsValid) // according to the tags of Model Class
+            {
+                _dbContext.Categories.Update(newCategory);//we ask to add a new row in table
+                _dbContext.SaveChanges(); //Commit changes
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteCategory(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = await _dbContext.Categories.FirstOrDefaultAsync(u => u.Id == id.Value);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("DeleteCategory")]
+        public async Task<IActionResult> DeleteCategoryPost(int? id)
+        {
+            var category = await _dbContext.Categories.FirstOrDefaultAsync(u => u.Id == id!.Value);
+
+            if (category == null)
+            {
+                return NotFound(); 
+            }
+
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges(); //Commit changes
+            return RedirectToAction("Index");
            
         }
     }
